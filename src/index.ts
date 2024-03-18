16939d0b319f216985168357eff07b65c87345e4
Abhishek-cipl <145000189+Abhishek-cipl@users.noreply.github.com>
2024-03-18 16:44:34 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, {Request, Response} from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRouter from "./routes/MyUserRoute";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log(`connected to DB`))
+  .catch(err => console.log(`connection failed ${err}`))
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/health", async(req: Request, res: Response) => res.json({ message: "Hello FoodJagat.com" }));
+
+app.use("/api/my/user", myUserRouter);
+
+app.listen(5000, () =>  console.log("server runs at port 5000"));
